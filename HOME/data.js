ec88efb2cdd4eda6de670babba8ef72bff550996
bigchicken1201 <yichangchen1201@gmail.com>
2024-11-08 11:12:50 +0800

    nhg

---
+++ HOME/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "寧安公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.0852183360833845,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.1073823064182555,
+          "pitch": -0.011294866733152276,
+          "title": "籃球場",
+          "text": "這個球場從我國小就在了，乘載著許多回憶"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
---
+++ xinsheng/data.js
@@ -0,0 +1,255 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-4",
+      "name": "新生公園4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.237514861154459,
+          "pitch": 0.19938683182315486,
+          "rotation": 0,
+          "target": "2-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4929960501040842,
+          "pitch": 0.002797231502663422,
+          "title": "簡介",
+          "text": "<div>兩面全場，位於松江路＆民族東路交叉口 夜間照明不限時，於場邊可自行開關&nbsp;</div><div>【交通資訊】&nbsp;</div><div>1. [淡水線圓山站]1號出口出站，轉乘紅50於「新生公園」站下車 ，或出站後沿玉門街直行，於民族路口左轉民族西路往東直行，步行約15分鐘可抵達。&nbsp;</div><div>2. [中和新蘆線行天宮站]3號出口出站，左轉沿松江路直走即可抵達。</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "新生公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.486064608053038,
+          "pitch": 0.11622378463716387,
+          "rotation": 0,
+          "target": "2-1"
+        },
+        {
+          "yaw": 3.096197895385883,
+          "pitch": 0.3496338343475749,
+          "rotation": 0.7853981633974483,
+          "target": "3-2"
+        },
+        {
+          "yaw": 0.33595065288518455,
+          "pitch": -0.008350627004334754,
+          "rotation": 0,
+          "target": "4-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15462921521081618,
+          "pitch": -0.015789743557689917,
+          "title": "評論",
+          "text": "****<div>燈可自行開關，唯獨大眾運輸比較不方便</div><div>*****</div><div>早上都有人負責打掃，將晚上沒水準的人所丟的垃圾清乾淨</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-1",
+      "name": "新生公園1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6988282718926797,
+          "pitch": 0.037141378124317725,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.1487502461466299,
+          "pitch": -0.022503625199533417,
+          "rotation": 0,
+          "target": "3-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9806744679332997,
+          "pitch": -0.10727545739406352,
+          "title": "相關影片",
+          "text": "https://www.youtube.com/watch?v=Mx4Jj38h7TI"
+        }
+      ]
+    },
+    {
+      "id": "3-2",
+      "name": "新生公園2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.207101197274426,
+          "pitch": 0.2739441585273532,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.0220756567837146,
+          "pitch": 0.31339874780272936,
+          "rotation": 0,
+          "target": "2-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6783424154472684,
+          "pitch": 0.012380706632946215,
+          "title": "為啥很有名",
+          "text": "因為交通方便，照明足夠，吸引各方籃球愛好者，也不時有名人或職業球員前來。"
+        }
+      ]
+    },
+    {
+      "id": "4-3",
+      "name": "新生公園3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.178352003656924,
+          "pitch": 0.0387381351571765,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.6844871153392402,
+          "pitch": 0.20420346488613816,
+          "rotation": 7.0685834705770345,
+          "target": "0-4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
